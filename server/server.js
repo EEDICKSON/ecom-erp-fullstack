c3ee8201f522e138d5c0a08422009ef3cb3d366d
EEDICKSON <edickson113@rku.ac.in>
2025-08-25 08:38:55 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const authRouter = require('./routes/auth/auth-routes')
 
 
 // Create a database connection - > u can also  
@@ -21,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: 'http://localhost:5173/',
+        origin: 'http://localhost:5173',
         methods: ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders: [
             "Content-Type",
@@ -36,5 +37,6 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on post ${PORT}`));
